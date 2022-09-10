764f29738eafd0b8021be2e129e2ff47ddce9c7a
Yaroslav Bodiak <ybodiak@vmware.com>
2022-09-10 21:40:12 +0300

    investment calculator

---
+++ src/main/java/invest/CompoundInterestCalculator.java
@@ -0,0 +1,81 @@
+package invest;
+
+public class CompoundInterestCalculator {
+
+    private double startSum;            //USD
+    private double annualMargin;        //%
+    private double monthlyTopUp;        //USD
+    private int yearsPeriod;            //years
+
+    public static void main(String[] args) {
+        CompoundInterestCalculator calculator = new CompoundInterestCalculator();
+        calculator.setStartSum(10000)
+                .setAnnualMargin(10)
+                .setMonthlyTopUp(0)
+                .setYearsPeriod(10);
+        calculator.getOverAllSumInYears();
+    }
+
+    public double getOverAllSumInYears() {
+        if (annualMargin == 0 && monthlyTopUp > 0) {
+            int monthInYears = 12;
+            startSum += monthInYears * monthlyTopUp * yearsPeriod;
+            System.out.println("Total sum: " + startSum);
+            return startSum;
+        }
+        else if (annualMargin == 0 && monthlyTopUp == 0) {
+            System.out.println("Total sum: " + startSum);
+            return startSum;
+        }
+
+        for (int i = 0; i < yearsPeriod; i++) {
+            if (monthlyTopUp > 0) {
+                int monthInYears = 12;
+                for (int j = 0; j < monthInYears; j++) {
+                    startSum += (startSum * (annualMargin / monthInYears / 100)) + monthlyTopUp;
+                }
+            }
+            else {
+                startSum += startSum * annualMargin / 100;
+            }
+        }
+        System.out.println("Total sum: " + startSum);
+        return startSum;
+    }
+
+    public double getStartSum() {
+        return startSum;
+    }
+
+    public CompoundInterestCalculator setStartSum(double startSum) {
+        this.startSum = startSum;
+        return this;
+    }
+
+    public double getAnnualMargin() {
+        return annualMargin;
+    }
+
+    public CompoundInterestCalculator setAnnualMargin(double annualMargin) {
+        this.annualMargin = annualMargin;
+        return this;
+    }
+
+    public double getMonthlyTopUp() {
+        return monthlyTopUp;
+    }
+
+    public CompoundInterestCalculator setMonthlyTopUp(double monthlyTopUp) {
+        this.monthlyTopUp = monthlyTopUp;
+        return this;
+    }
+
+    public int getYearsPeriod() {
+        return yearsPeriod;
+    }
+
+    public CompoundInterestCalculator setYearsPeriod(int yearsPeriod) {
+        this.yearsPeriod = yearsPeriod;
+        return this;
+    }
+}
---
+++ src/test/java/invest/CompoundInterestTest.java
@@ -0,0 +1,67 @@
+package invest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CompoundInterestTest {
+
+    private CompoundInterestCalculator sut = new CompoundInterestCalculator();
+
+    @Test
+    void testNoMarginNoTopUp() {
+        sut.setStartSum(1000)
+                .setYearsPeriod(10);
+        assertEquals(1000, sut.getOverAllSumInYears());
+    }
+
+    @Test
+    void test1YearWithMargin10NoTopUp() {
+        sut.setStartSum(1000)
+                .setAnnualMargin(10)
+                .setYearsPeriod(1);
+        assertEquals(1100, sut.getOverAllSumInYears());
+    }
+
+    @Test
+    void test2YearsWithMargin12NoTopUp() {
+        sut.setStartSum(1000)
+                .setAnnualMargin(12)
+                .setYearsPeriod(2);
+        assertEquals(1254.4, sut.getOverAllSumInYears());
+    }
+
+    @Test
+    void test3YearsWithMarginNoTopUp() {
+        sut.setStartSum(1000)
+                .setAnnualMargin(10)
+                .setYearsPeriod(3);
+        assertEquals(1331, sut.getOverAllSumInYears());
+    }
+
+    @Test
+    void test1YearWithMarginWithTopUp() {
+        sut.setStartSum(1000)
+                .setAnnualMargin(12)
+                .setYearsPeriod(1)
+                .setMonthlyTopUp(100);
+        assertEquals(2395, (int) sut.getOverAllSumInYears());
+    }
+
+    @Test
+    void test2YearsWithMarginWithTopUp() {
+        sut.setStartSum(1000)
+                .setAnnualMargin(12)
+                .setYearsPeriod(2)
+                .setMonthlyTopUp(100);
+        assertEquals(3967, (int) sut.getOverAllSumInYears());
+    }
+
+    @Test
+    void test2YearsNoMarginWithTopUp() {
+        sut.setStartSum(1000)
+                .setYearsPeriod(2)
+                .setMonthlyTopUp(100);
+        assertEquals(3400, (int) sut.getOverAllSumInYears());
+    }
+}
