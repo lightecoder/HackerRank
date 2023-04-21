bc1b258a09567cfacf07ae2fd5eefe889157f829
bodiaky <y.bodiak@elsevier.com>
2023-04-21 01:28:48 -0400

    commit

--- src/main/java/dmv/MyHttpServer.java
+++ src/main/java/dmv/MyHttpServer.java
@@ -1,20 +1,22 @@
 package dmv;
 
-import javax.print.attribute.standard.Media;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import javafx.application.Application;
+import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
 
-public class MyHttpServer {
+import java.io.File;
+
+public class MyHttpServer extends Application {
     public static void main(String[] args) {
-        new MyHttpServer().call();
+        launch(args);
+//        new MyHttpServer().call();
     }
 
     private void call() {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        Runnable task = () -> {
-            try {
+//        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+//        Runnable task = () -> {
+//            try {
 //                URL url = new URL("https://nqapflhsmv.nemoqappointment.com/wp-admin/admin-ajax.php?location=17&service=5&worker=3&action=ea_month_status&month=6&year=2023&check=ed65addf52");
 //                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 //                connection.setRequestMethod("GET");
@@ -31,18 +33,28 @@ public class MyHttpServer {
 //                System.out.println("Response code: " + responseCode + ", status = " + status);
 
 //                if (!status.split(":")[1].equals("no-slot")) {
-                String audioFile = "music.mp3";
-                Media media = new Media(getClass().getResource(audioFile).toExternalForm());
+                Media media = new Media(new File("src/main/resources/mus.mp3").toURI().toString());
                 MediaPlayer mediaPlayer = new MediaPlayer(media);
                 mediaPlayer.play();
 //                }
 //                connection.disconnect();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        };
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        };
 
-        scheduler.scheduleAtFixedRate(task, 0, 10, TimeUnit.SECONDS);
+//        scheduler.scheduleAtFixedRate(task, 0, 10, TimeUnit.SECONDS);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //"src/main/resources/mus.mp3"
+        String path = getClass().getResource("mus.mp3").getPath();
+        System.out.println(path);
+        Media media = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+//        primaryStage.setTitle("Music Player");
+//        primaryStage.show();
+    }
 }
