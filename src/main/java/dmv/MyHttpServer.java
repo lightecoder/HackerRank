99732dc424080cdf91f59799fd73fc9e84d846f7
lightecoder <lightecoder@gmail.com>
2023-04-21 03:52:30 -0400

    tasks MDV

--- src/main/java/dmv/MyHttpServer.java
+++ src/main/java/dmv/MyHttpServer.java
@@ -4,57 +4,66 @@ import javafx.application.Application;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MyHttpServer extends Application {
     public static void main(String[] args) {
-        launch(args);
-//        new MyHttpServer().call();
+        new MyHttpServer().call(args);
     }
 
-    private void call() {
-//        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-//        Runnable task = () -> {
-//            try {
-//                URL url = new URL("https://nqapflhsmv.nemoqappointment.com/wp-admin/admin-ajax.php?location=17&service=5&worker=3&action=ea_month_status&month=6&year=2023&check=ed65addf52");
-//                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//                connection.setRequestMethod("GET");
-//                int responseCode = connection.getResponseCode();
-//
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                String line;
-//                StringBuilder response = new StringBuilder();
-//                while ((line = reader.readLine()) != null) {
-//                    response.append(line);
-//                }
-//                reader.close();
-//                String status = response.substring(response.indexOf("2023-06-19"), (response.indexOf("2023-06-19") + 21)).replaceAll("\"", "");
-//                System.out.println("Response code: " + responseCode + ", status = " + status);
+    private void call(String[] args) {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        Runnable task = () -> {
+            try {
+                URL url = new URL("https://nqapflhsmv.nemoqappointment.com/wp-admin/admin-ajax.php?location=17&service=5&worker=3&action=ea_month_status&month=6&year=2023&check=ed65addf52");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                int responseCode = connection.getResponseCode();
 
-//                if (!status.split(":")[1].equals("no-slot")) {
-                Media media = new Media(new File("src/main/resources/mus.mp3").toURI().toString());
-                MediaPlayer mediaPlayer = new MediaPlayer(media);
-                mediaPlayer.play();
-//                }
-//                connection.disconnect();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        };
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
 
-//        scheduler.scheduleAtFixedRate(task, 0, 10, TimeUnit.SECONDS);
+                String date = "2023-06-21";
+                String status = response.substring(response.indexOf(date), (response.indexOf(date) + 21)).replaceAll("\"", "");
+                System.out.println("Response : " + response);
+                System.out.println("Response code: " + responseCode + ", status = " + status + "\n");
+
+                if (!status.split(":")[1].equals("no-slots")) {
+                    for (int i = 0; i < 100; i++) {
+                        launch(args);
+                    }
+                } else {
+                    System.out.println("Still not available .. ");
+                }
+                connection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+        scheduler.scheduleAtFixedRate(task, 0, 10, TimeUnit.SECONDS);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //"src/main/resources/mus.mp3"
         String path = this.getClass().getResource("/mus.mp3").getPath();
-        System.out.println(path);
         Media media = new Media(new File(path).toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
         mediaPlayer.play();
-//        primaryStage.setTitle("Music Player");
-//        primaryStage.show();
     }
 }
